986fe054ef13d70726b209542fc56a129fae034b
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-14 10:07:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Q no 10
+// MY name is Safdar Ali Khan
+// Date 11 -03 -24
+console.log("Allhumdurillah Q no 10 is done with addiing comments");
+// Q no 11
+var FriendsNames1 = ["Amir", "Wakeel Tareen", "Adil", "Raheel"];
+for (var _i = 0, FriendsNames1_1 = FriendsNames1; _i < FriendsNames1_1.length; _i++) {
+    var FriendsName1 = FriendsNames1_1[_i];
+    console.log("".concat(FriendsName1));
+}
+// Q no 12
+var FriendsNames2 = ["Amir", "Wakeel Tareen", "Adil", "Raheel"];
+for (var _a = 0, FriendsNames2_1 = FriendsNames2; _a < FriendsNames2_1.length; _a++) {
+    var FriendsName2 = FriendsNames2_1[_a];
+    console.log("Hello ".concat(FriendsName2, " Whats going on"));
+}
---
+++ main.ts
@@ -0,0 +1,18 @@
+// Q no 10
+
+// MY name is Safdar Ali Khan
+// Date 11 -03 -24
+console.log("Allhumdurillah Q no 10 is done with addiing comments");
+
+
+// Q no 11
+let FriendsNames1 : string[] =["Amir","Wakeel Tareen","Adil","Raheel"]
+for (let FriendsName1 of FriendsNames1){
+    console.log(`${FriendsName1}`);
+}
+
+// Q no 12
+let FriendsNames2 : string[] =["Amir","Wakeel Tareen","Adil","Raheel"]
+for (let FriendsName2 of FriendsNames2){
+    console.log(`Hello ${FriendsName2} Whats going on`);
+}
